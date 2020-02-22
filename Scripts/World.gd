89cc2ef1818c0e4fd1c2f1868be580cd22586d66
amanzur16 <59851844+amanzur16@users.noreply.github.com>
2020-02-21 22:40:56 -0500

    Updates to the file

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,17 @@
 extends Node2D
+var _trauma
 
 onready var Camera = $Camera
 
 func _ready():
-	pass
+	_trauma = 0.0
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("Up"):
+		$Camera.add_trauma(0.25)
+	if Input.is_action_pressed("Down"):
+		$Camera.add_trauma(0.25)
+	if Input.is_action_pressed("Right"):
+		$Camera.add_trauma(0.25)
+	if Input.is_action_pressed("Left"):
+		$Camera.add_trauma(0.25)
